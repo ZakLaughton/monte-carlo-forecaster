ae8cf0312d1a58d9ae6c4c45c1bb3f74df4841d9
Zak Laughton <zlaughton@gmail.com>
2026-02-03 18:54:12 -0600

    add project size

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,19 @@ import "./App.css";
 
 function App() {
   const [pastVelocity, setPastVelocity] = useState<number[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
+  const [velocityInput, setVelocityInput] = useState<string>("");
+
+  const [projectSize, setProjectSize] = useState<number>(20);
+  const [projectSizeInput, setProjectSizeInput] = useState<number>("20");
 
   const handleSubmit = (event: React.SubmitEvent) => {
     event.preventDefault();
 
-    if (!inputValue) return;
-
-    setPastVelocity(inputValue.split(",").map((num) => Number(num)));
+    const parsedSize = Number(projectSizeInput);
+    if (!Number.isNaN(parsedSize) && parsedSize >= 0) {
+      setProjectSize(parsedSize);
+    }
+    setPastVelocity(velocityInput.split(",").map((num) => Number(num)));
   };
 
   return (
@@ -20,20 +25,31 @@ function App() {
       <h1>Forecasting tool</h1>
 
       <form onSubmit={handleSubmit}>
-        <label>
+        <label style={{ display: "block" }}>
           Add past sprint velocity. Use the number of tickets moved to "done"
           each week. Format: 1,3,4,3,2.
           <input
             type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            value={velocityInput}
+            onChange={(e) => setVelocityInput(e.target.value)}
+          />
+        </label>
+        <label>
+          Project size (number of stories)
+          <input
+            type="number"
+            value={projectSizeInput}
+            onChange={(e) => setProjectSizeInput(Number(e.target.value))}
           />
         </label>
         <button type="submit">Add</button>
       </form>
 
       <div>
-        <h3>History</h3>
+        <h3>Data</h3>
+        <h4>Project size</h4>
+        {projectSize}
+        <h4>Velocity</h4>
         {pastVelocity.length === 0 ? (
           <p>No data yet.</p>
         ) : (
