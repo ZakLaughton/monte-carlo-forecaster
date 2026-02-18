0793be92fc18a3044b3dfa5d789aa5fd42dad4ca
Zak Laughton <zlaughton@gmail.com>
2026-02-17 19:01:49 -0600

    refactor: set default color scheme for MantineProvider to dark

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="dark">
       <App />
     </MantineProvider>
   </StrictMode>,
